6ef3d2aa02cbfd3ffe0d1ea55fc64a45ad8ae681
Umesh <97531471+umeshyadav7988@users.noreply.github.com>
2024-05-31 01:53:47 +0530

    bubble_sort.cpp

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -8,7 +8,7 @@ void swap(int *a, int *b)
 void bubble(int *arr, int n)
 {
     int i, j;
-    for (int i = 0; i < n - 1; i++)
+    for ( int i = 0; i < n - 1; i++)
     {
         for (int j = 0; j < n - i - 1; j++)
         {
